4279e5c8e8ac980972bb3a12b74b1c2b4800ba4c
Rasen Nikov <rasen800@gmail.com>
2023-11-06 20:23:50 +0600

    изменил "Промах" - "T"

--- studies/oop/module_c2/c2_5/war/sea_war.py
+++ studies/oop/module_c2/c2_5/war/sea_war.py
@@ -121,7 +121,7 @@ class Board:
                     self.last_hit.append(d)
                     return True
 
-        self.field[d.x][d.y] = '.'
+        self.field[d.x][d.y] = 'T'
         print('Мимо!')
         return False
 
