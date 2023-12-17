31613c8537c7f5302c59e580307a786cabe154a4
Rasen Nikov <rasen800@gmail.com>
2023-12-17 16:03:08 +0600

    Обновление модулей d2, d3, d4

--- studies/oop/proba/temp.py
+++ studies/oop/proba/temp.py
@@ -13,4 +13,23 @@ def friend_name():
     print('Друг Олег')
 
 
+print('------------\n')
+DATA_FILL = (
+    ("Active", "Активный"),
+    ("Inactive", "Неактивный")
+)
+tup = tuple(x for x in DATA_FILL)
+print('tup:', tup)
 
+
+qw = tuple(t for tup in DATA_FILL for t in tup)
+print('qw', qw)
+
+print('проба: ', qw[0], ' ', qw[1], ' ', qw[2])
+# for tup in DATA_FILL:
+#     for t in tup:
+#         print('t:', t)
+#
+for x in DATA_FILL:
+    name0, name1 = x[0], x[1]
+    print('name:', name0, 'name1:', name1)
